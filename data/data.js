bceb35de83f1563cbf3c1420346995aff68f9fde
rodages <zanasmakarov@gmail.com>
2022-06-12 22:53:44 +0100

    uploaded favicon and fixed typos

--- data/data.js
+++ data/data.js
@@ -9,16 +9,16 @@ const data = {
         description:'description',
         currentlyLearning: '',
         interests:[
-            {Sports : `Morning workouts or pre-work runs with a goal of comfotably making 10k.
+            {Sports : `Morning workouts or pre-work runs with a goal of comfortably making 10k.
             Attending intermediate pick-up basketball games.
-            Organising volleyball games with workmates in the summer and Archery sessions in cold seasons.
+            Organising volleyball games with workmates in the summer and archery sessions in cold seasons.
             ` },
-            {Travelling: "Have not travelled at all before coming to the UK. However, after graduating from university in 2016 have managed to visit 16 countries in 4 years. Currently, most trips have been to Europe. Before exploring other continents, I cannot wait to visit Iceland to see the Northern Lights and Nazare in Portugal to see their famous huge waves!"},
+            {Travelling: "I have not travelled at all before coming to the UK. However, after graduating from university in 2016 I have managed to visit 16 countries in 4 years. Currently, most of the trips have been to Europe. Before exploring other continents, I cannot wait to visit Iceland to see the Northern Lights and Nazare in Portugal to see their famous huge waves!"},
             {"Educational Content":`Following Coding / History / Geopolitical / Economics / Geographics channels on YouTube.`},
         ],
         furtherLearning:[
-            {Typescript:'We have briefly covered introduction to Typescript towards the end of the course. While writting code I trying to think of edge case scenarios where code could break. Typescript seems like a great tool and solution to tackle the unforseen bugs.'},
-            {NextJS:'I have recently attended "Free Code Camp" networking event, where most people working in the field were advising me on learning more about Next now that I know react. I have built this portfolio with Next and can see that the site loading times are much faster compared to my previous projects and would like to understand the technology a bit better'},
+            {Typescript:'We have briefly covered introduction to Typescript towards the end of the course. While writting code I am trying to think of edge case scenarios where code could break. Typescript seems like a great tool and solution to tackle the unforseen bugs.'},
+            {NextJS:'I have recently attended "Free Code Camp" networking event, where most people working in the field were advising me on learning more about Next now that I know React. I have built this portfolio with Next and can see that the site loading times are much faster compared to my previous projects and would like to understand the technology a bit better.'},
             {GraphQL:'While working with RESTful APIs I have noticed that sometimes you need to disregard most of the fetched data, which seems a bit wasteful. I would like to learn more about GraphQL as a query driven alternative to REST APIs.'}
         ]
     },
@@ -33,17 +33,17 @@ const data = {
             name:'The Company Planner',
             technologies:['React', 'JavaScript', 'MaterialUI', 'CSS3', 'Django', 'Python', 'PostgreSQL', 'Postman', 'Heroku'],
             timeframe:'2 Weeks',
-            description:"A solo project built over two weeks aimed at reducing paper wastage at an existing employment. I started off by building CMS-ish platform where all components could be custumisable, but due to time constraints and certain bugs coming out - I had to change it towards a more tailored route.The backend is built with Django REST framework and PostgreSQL, while frontend is done with React and MaterialUI.",
+            description:"A solo project built over two weeks aimed at reducing paper wastage at the existing employment. I started off by building CMS-ish platform where all components could be customisable, but due to time constraints and certain bugs coming out - I had to change it towards a more tailored route. The backend is built with Django REST framework and PostgreSQL, while frontend is done with React and MaterialUI.",
             setup:'Solo',
             github:'https://bit.ly/3NBbZRg',
-            deployedLink:'https://bit.ly/3zvNlNF',
+            deployedLink:'https://companyplanner.netlify.app/',
         },
         {
             key:'SEIProject3',
             name:'Find Me A Doc',
             technologies:['React', 'JavaScript', 'Bootstrap', 'CSS3', 'Node', 'Express', 'MongoDB', 'Postman', 'Git', 'Heroku', 'Trello' , 'Slack'],
             timeframe:'2 Weeks',
-            description:"A MERN stack application built in a group of three over the period of two weeks aimed at creating a platform to connect doctors/specialists to patients. I was responsible for building doctor and patient CRUD routes on the backend and Navbar state, editing routes, working with MapboxAPI to get doctors' postcodes and reviewing functionality on the frontend.",
+            description:"A MERN stack application built in a group of three over the period of two weeks aimed at creating a platform to connect doctors/ specialists to patients. I was responsible for building the doctor and patient CRUD routes on the backend and Navbar state, editing routes, working with MapboxAPI to get doctors' postcodes and 'writting reviews' functionality on the frontend.",
             setup:'Group',
             github:'https://bit.ly/3xxuwZ2',
             deployedLink:'https://bit.ly/3zrB7FF',
@@ -63,7 +63,7 @@ const data = {
             name:'The Pandemic Game',
             technologies:['JavaScript', 'HTML5' , 'CSS3'],
             timeframe:'2 Weeks',
-            description:'a solo game project built over two weeks inspired by events that started in 2020 and reached the peak exactly during the beginning of the coding bootcamp. I decided to make a custom game rather than selecting one of the suggested common projects like space invaders, Tetris and battleships. The game ended up having lots of state bringing together a handful of bugs. However, the project and the challenges helped me to realise that I definitely want to work in this field.',
+            description:'A solo game project built over two weeks inspired by COVID19 and reached the peak during the beginning of the coding bootcamp. I decided to make a custom game rather than selecting one of the suggested common projects like space invaders, Tetris and battleships. The game ended up having lots of state bringing together a handful of bugs. However, the project and the challenges helped me to realise that I definitely want to work in this field!',
             setup:'Solo',
             github:'https://bit.ly/3mBheEs',
             deployedLink:'https://bit.ly/3mu37AO',
@@ -75,7 +75,7 @@ const data = {
             company:'General Assembly',
             year:'2021 November - 2022 May',
             // companyLink:'link',
-            description:'A high-intensity 24-week part-time immersive course at General Assembly designed for people working full time to learn and practice necessary skills to transition their career to tech as full-stack software engineers. The course involved daily stand-ups, online lessons, labs, pair-coding, whiteboarding and four individual/ group projects. The key takeaways from the course are improved technical communication, pair coding and whiteboarding, better time management while working on coding-related projects and understanding that I can quickly learn new libraries/frameworks required for the project.',
+            description:'A high-intensity 24-week part-time immersive course at General Assembly designed for people working full time to learn and practice necessary skills to change their career to tech as full-stack software engineers. The course involved daily stand-ups, online lessons, labs, pair-coding, whiteboarding and four individual/ group projects. The key takeaways from the course are improved technical communication, pair coding and whiteboarding, better time management while working on coding-related projects and understanding that I can quickly learn new libraries/frameworks required for the project.',
             image:'logoGA'
         },
         {
@@ -99,7 +99,7 @@ const data = {
             company:'Middlesex University',
             year:'October 2013 - May 2016',
             // companyLink:'link',
-            description:'Graduated with a 2:1 in BSc Psychology. The course has emphasized Research Methods and Data Analysis throughout the whole degree and covered various different approaches to counceling. This helped me to improve my academic writing, working with big data sets and carry out research. In particular I was very interested in modules of Positive psychology and Coaching that have helped a lot whilst working in management roles later in my career.',
+            description:'Graduated with a 2:1 in BSc Psychology. The course has emphasized Research Methods and Data Analysis throughout the whole degree and covered various different approaches to counselling. This helped me to improve my academic writing, working with big data sets and carry out research. In particular I was very interested in modules of Positive Psychology and Coaching that have helped me a lot whilst working in management roles later in my career.',
             image:'logoMiddlesex'
         },
     ],
